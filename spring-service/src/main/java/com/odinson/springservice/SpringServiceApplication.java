94e53583f9837c38f6b6a5a69a97e46da8632191
Victor Ay <viktoray007@gmail.com>
2019-09-25 15:50:20 +0300

    Initial commit

---
+++ spring-service/src/main/java/com/odinson/springservice/SpringServiceApplication.java
@@ -0,0 +1,14 @@
+package com.odinson.springservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceApplication.class, args);
+    }
+
+}
